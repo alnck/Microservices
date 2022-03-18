37499b473a2501f41550bef643becefd75803f53
Eren <alincakeren@gmail.com>
2022-03-18 12:36:35 +0300

    first commit

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  internal class CoursesController : ControllerBase
+  {
+    private readonly ICourseService _courseService;
+
+    public CoursesController(ICourseService courseService)
+    {
+      _courseService = courseService;
+    }
+
+    public async Task<IActionResult> GetById(string id)
+    {
+      var response = await _courseService.GetByIdAsync(id);
+    }
+  }
+}
---
+++ Shared/FreeCourse.Shared/Dtos/Response.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+  public class Response<T>
+  {
+    public T Data { get; private set; }
+
+    public List<string> Errors { get; set; }
+
+    [JsonIgnore]
+    public int StatusCode { get; private set; }
+    [JsonIgnore]
+    public bool IsSuccessful { get; private set; }
+
+    // Static Factory Methodlar 
+
+    public static Response<T> Success(T data, int statucCode)
+    {
+      return new Response<T>
+      {
+        Data = data,
+        StatusCode =
+        statucCode,
+        IsSuccessful = true
+      };
+    }
+
+    // Update ve Delete işlemlerinde
+    public static Response<T> Success(int statusCode)
+    {
+      return new Response<T>
+      {
+        Data = default(T),
+        StatusCode = statusCode,
+        IsSuccessful = true
+      };
+    }
+
+    // Hata durumunda
+    public static Response<T> Fail(List<string> errors, int statusCode)
+    {
+      return new Response<T>
+      {
+        Errors = errors,
+        StatusCode = statusCode,
+        IsSuccessful = true
+      };
+    }
+
+    public static Response<T> Fail(string error, int statusCode)
+    {
+      return new Response<T>
+      {
+        Errors = new List<string>() { error },
+        StatusCode = statusCode,
+        IsSuccessful = true
+      };
+    }
+  }
+}
