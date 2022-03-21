539af3cdd082e15f796a595ad46ac1a5f3ede3a3
alnck <alincakeren@gmail.com>
2022-03-21 11:22:56 +0300

    identity server update

--- IdentityServer/FreeCourse.IdentityServer/Models/ApplicationUser.cs
+++ IdentityServer/FreeCourse.IdentityServer/Models/ApplicationUser.cs
@@ -2,8 +2,9 @@
 
 namespace FreeCourse.IdentityServer.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+  // Add profile data for application users by adding properties to the ApplicationUser class
+  public class ApplicationUser : IdentityUser
+  {
+    public string City { get; set; }
+  }
 }
--- IdentityServer/FreeCourse.IdentityServer/Program.cs
+++ IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using FreeCourse.IdentityServer.Data;
+using FreeCourse.IdentityServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,68 +18,68 @@ using System.Linq;
 
 namespace FreeCourse.IdentityServer
 {
-    public class Program
+  public class Program
+  {
+    public static int Main(string[] args)
     {
-        public static int Main(string[] args)
+      Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug()
+          .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+          .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+          .MinimumLevel.Override("System", LogEventLevel.Warning)
+          .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+          .Enrich.FromLogContext()
+          // uncomment to write to Azure diagnostics stream
+          //.WriteTo.File(
+          //    @"D:\home\LogFiles\Application\identityserver.txt",
+          //    fileSizeLimitBytes: 1_000_000,
+          //    rollOnFileSizeLimit: true,
+          //    shared: true,
+          //    flushToDiskInterval: TimeSpan.FromSeconds(1))
+          .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
+          .CreateLogger();
+
+      try
+      {
+        var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
-                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                // uncomment to write to Azure diagnostics stream
-                //.WriteTo.File(
-                //    @"D:\home\LogFiles\Application\identityserver.txt",
-                //    fileSizeLimitBytes: 1_000_000,
-                //    rollOnFileSizeLimit: true,
-                //    shared: true,
-                //    flushToDiskInterval: TimeSpan.FromSeconds(1))
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
-                .CreateLogger();
+          var serviceProvider = scope.ServiceProvider;
 
-            try
-            {
-                var seed = args.Contains("/seed");
-                if (seed)
-                {
-                    args = args.Except(new[] { "/seed" }).ToArray();
-                }
+          var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
 
-                var host = CreateHostBuilder(args).Build();
+          applicationDbContext.Database.Migrate();
 
-                if (seed)
-                {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
-                }
+          var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-                Log.Information("Starting host...");
-                host.Run();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly.");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+          if (userManager.Users.Any())
+          {
+            userManager.CreateAsync(new ApplicationUser { UserName = "Admin", Email = "admin@xxx.com", City = "admincity" }, "Password12*").Wait();
+          }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Log.Information("Starting host...");
+        host.Run();
+        return 0;
+      }
+      catch (Exception ex)
+      {
+        Log.Fatal(ex, "Host terminated unexpectedly.");
+        return 1;
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
